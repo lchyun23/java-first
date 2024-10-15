49df7d4863f76b992ebf9e0697899b41c91d061f
lchyun23 <lchyun23@gmail.com>
2024-10-15 21:37:08 +0900

    4개 필드 + 생성자

---
+++ src/main/java/com/example/demo/Member.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+public class Member {
+
+  private Integer id;
+  private String name;
+  private Integer age;
+  private String email;
+
+  public Member(Integer id, String name, Integer age, String email) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.email = email;
+  }
+}
