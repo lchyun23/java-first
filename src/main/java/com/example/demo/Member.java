0e686dca6bd3251d3ea5be8305b6e68a617c8d86
lchyun23 <lchyun23@gmail.com>
2024-10-16 16:04:47 +0900

    lombok 설정

--- src/main/java/com/example/demo/Member.java
+++ src/main/java/com/example/demo/Member.java
@@ -21,33 +21,4 @@ public class Member {
     this.age = age;
     this.email = email;
   }
-
-  // Getter 생성
-  // @Getter, @Setter를 쓰면은 24~43 줄이 필요 없다.
-  public int getId() {
-    return id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public Integer getAge() {
-    return age;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  // Setter
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  // 객체 내부에 있는 필드에 접근할 때는 get 필요 없다.
-  // @toString으로 대체할 수 있다.
-  public String toString() {
-    return ("id: " + this.id + "name: " + this.name + "age: " + getAge() + "email: " + getEmail());
-  }
 }
